c7d7fe1462a3f8cca39a3ff654028a0fa2b2c768
Daniel Raymond <dan@raymond.ch>
2020-02-01 18:17:37 -0500

    Showing Playlist Selection!

--- vibeify/src/pages/settings.js
+++ vibeify/src/pages/settings.js
@@ -1,43 +1,77 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState, useContext } from 'react';
 import { Checkbox } from '@material-ui/core'
 import spotify_logo from '../assets/spotify_logo.svg'
+import Store from "../store";
 
 
-class SettingsPage extends Component {
-  state = {
+const SettingsPage = () => {
+  const { spotifyToken } = useContext(Store);
+
+  const [state, setState] = useState({
     selectedIDs: [],
-    playlists: [
-      {title: "Jazz", length: "2h 56m", onPress: () => {this.editIds(0)}},
-      {title: "Funk", length: "1h 34m", onPress: () => {this.editIds(1)}},
-    ],
-  }
+  });
+  const [playlists, setPlaylistOptions] = useState([]);
 
-  editIds(id) {
-    let currentIDs = this.state.selectedIDs
+  useEffect(() => {
+    if (!playlists.length) {
+      const fetchData = async () => {
+        const endpoint = "https://api.spotify.com/v1/me/playlists";
+        const result = await fetch(endpoint, {
+          headers: {
+            Authorization: "Bearer " + spotifyToken
+          }
+        })
+          .then(r => r.json())
+          .catch(e => alert(`Error: ${JSON.stringify(e)}`));
+ 
+          setPlaylistOptions(result.items.map(element => { 
+          return ({
+            title: element.name,
+            length: element.tracks.total,
+            onPress: () => editIds(element.id)
+          })
+        }));
+      };
+      fetchData();
+    }
+  });
+
+
+  const editIds = (id) => {
+    let currentIDs = state.selectedIDs
     if (currentIDs.includes(id))
       currentIDs = currentIDs.filter((sID) => sID != id)
     else
       currentIDs.push(id)
 
-    this.setState({selectedIDs: currentIDs})
-    console.log(currentIDs)
-  }
+    setState({ selectedIDs: currentIDs })
+  };
 
-  render() {
-    return (
-      <div className="Settings-modal">
-        <table 
+  return (
+    <div className="Settings-modal">
+      <table
         style={{
           width: "80%",
           margin: "auto"
         }}>
-          {this.state.playlists.map((val, i) => <Playlist {...val}/>)}
-        </table>
-      </div>
-    );
-  }
+          <tr>
+            <td/>
+            <td>
+              <h3>Name</h3>
+            </td>
+            <td>
+              <h3>Number of Songs</h3>
+            </td>
+            <td>
+              <h3>Chosen</h3>
+            </td>
+          </tr>
+        {playlists.map((val, i) => <Playlist {...val} />)}
+      </table>
+    </div>
+  );
+};
 
-}
 
 class Playlist extends Component {
   render() {
