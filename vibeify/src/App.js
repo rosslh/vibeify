35d883dd5e4979c349ef89e05e0bbbbd726d4acd
Daniel Raymond <dan@raymond.ch>
2020-02-01 09:12:27 -0500

    Create different pages

--- vibeify/src/App.js
+++ vibeify/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import CameraPage from './pages/camera';
+import LoginPage from './pages/login'
+import SettingsPage from './pages/settings'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <CameraPage/>
+    <SettingsPage/>
+    // <LoginPage/>
   );
 }
 
---
+++ vibeify/src/pages/settings.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class SettingsPage extends Component {
+    render() {
+        return (
+            <header className="App-header">
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+        );
+    }
+}
+
+export default SettingsPage
