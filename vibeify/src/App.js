581c74289229b916967a49cd4c07a647aff052f0
Ross Hill <rosshill@protonmail.com>
2020-02-01 09:51:47 -0500

    merge

--- vibeify/src/App.js
+++ vibeify/src/App.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import './App.css';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router'
-import CameraPage from './pages/camera';
-import LoginPage from './pages/login'
-import SettingsPage from './pages/settings'
+import React from "react";
+import "./App.css";
+import { Router, Route, browserHistory, IndexRoute } from "react-router";
+import CameraPage from "./pages/camera";
+import LoginPage from "./pages/login";
+import SettingsPage from "./pages/settings";
 
 function App() {
   return (
     <Router history={browserHistory}>
-      <Route path ="/">
-          <IndexRoute component={LoginPage}/>
-          <Route path={"home"} component={LoginPage}/>
-          <Route path={"play"} component={CameraPage}/>
-          <Route path={"settings"} component={SettingsPage}/>
+      <Route path="/">
+        <IndexRoute component={LoginPage} />
+        <Route path={"home"} component={LoginPage} />
+        <Route path={"play"} component={CameraPage} />
+        <Route path={"settings"} component={SettingsPage} />
       </Route>
     </Router>
     // <CameraPage/>
