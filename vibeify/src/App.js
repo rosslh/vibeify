9b8ad986261c19b4a4a55018cd187ce2f530d04e
Daniel Raymond <dan@raymond.ch>
2020-02-01 21:00:12 -0500

    Deleted

--- vibeify/src/App.js
+++ vibeify/src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import CameraPage from "./pages/camera/camera";
 import LoginPage from "./pages/login";
-import PlaylistsPage from "./pages/playlists";
-// import SettingsPage from "./pages/settings";
 import StoreProvider from "./storeProvider";
 
 function App() {
@@ -15,8 +13,6 @@ function App() {
           <IndexRoute component={LoginPage} />
           <Route path={"home"} component={LoginPage} />
           <Route path={"play"} component={CameraPage} />
-          <Route path={"playlists"} component={PlaylistsPage} />
-          {/* <Route path={"settings"} component={SettingsPage} /> */}
         </Route>
       </Router>
     </StoreProvider>
