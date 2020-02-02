6b99568f516c27af1d23c40bf87a7c40160b06b0
Daniel Raymond <dan@raymond.ch>
2020-02-01 21:48:20 -0500

    Shrunk NavBar

--- vibeify/src/components/navBar.js
+++ vibeify/src/components/navBar.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import headerGraphic from '../assets/headerGraphic.png';
 import { Button } from '@material-ui/core';
-import { browserHistory } from 'react-router';
 import settings from '../assets/icons8-settings.svg';
 import Modal from 'react-modal'
 import SettingsPage from "../pages/settings";
+import Player from "../components/player";
 
 class Nav extends Component {
     state = {
@@ -13,41 +13,45 @@ class Nav extends Component {
 
     render() {
         const headerText = {
-            color:"white",
+            color: "white",
             textAlign: "left",
             margin: "auto",
-            marginTop: 0,
-            fontSize: 100,
+            marginTop: -4,
+            fontSize: 60,
+            paddingRight: "2rem"
         };
 
         let visible = this.props.isLoggedIn ? "visible" : "hidden"
 
         return (
-            <div style={{ display: "flex", position: "absolute", alignItems: "center", width: "100%", justifyContent: "space-between", paddingRight: "16" }}>
+            <span>
+                <div style={{ display: "flex", position: "absolute", alignItems: "center", width: "100%", justifyContent: "space-between" }}>
+                    <span style={{ display: "flex", flexDirection: "row" }}>
+                        <img src={headerGraphic} alt="logo" height="65" width="80" />
+                        <h2 style={headerText}>vibeify</h2>
+                    </span>
 
-                <img src={headerGraphic} className="logo" alt="logo" height="65" width="80" />
-                <h1 style={headerText}>vibeify</h1>
-
-                <Button
-                    className="Login_button"
-                    onClick={() => { this.setState({ showModal: true }) }}
-                    variant="contained" color="0xfff"
-                    style={{visibility: visible, zIndex: 1000}}
-                >
-                    <img src={settings} className="Spotify_logo_login" height="30" width="40" />
-                </Button>
+                    <Button
+                        className="Login_button"
+                        onClick={() => { this.setState({ showModal: true }) }}
+                        variant="contained" color="0xfff"
+                        style={{ visibility: visible, zIndex: 1000 }}
+                    >
+                        <img src={settings} className="Spotify_logo_login" height="30" width="40" />
+                    </Button>
+                </div>
                 <div>
                     <Modal
                         isOpen={this.state.showModal}
                         onRequestClose={this.closeModal}
                     >
                         <div>
-                            <SettingsPage/>
+                            <SettingsPage />
                         </div>
                         <button onClick={() => { this.setState({ showModal: false }) }}>close</button>
                     </Modal>
                 </div>
-            </div>
+            </span>
         );
     }
 }
